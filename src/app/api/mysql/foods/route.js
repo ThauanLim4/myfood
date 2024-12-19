9708c879e79dc530516140be241855985635c881
ThauanLim4 <116463820+ThauanLim4@users.noreply.github.com>
2024-12-19 09:43:37 -0300

    TEST: testando banco de dados hospedado no railway

--- src/app/api/mysql/foods/route.js
+++ src/app/api/mysql/foods/route.js
@@ -1,16 +1,9 @@
 import mysql from "mysql2/promise";
 
 export async function GET(request) {
-    const dbConfig = {
-        host: "localhost",
-        user: "root",
-        password: "1234",
-        database: "foods"
-    };
-
     try {
 
-        const conection = await mysql.createConnection(dbConfig);
+        const conection = await mysql.createConnection("mysql://root:VnTcdxYndhugegcsgziTgEymdLfCcWZo@junction.proxy.rlwy.net:54287/railway");
         const [rows] = await conection.execute("SELECT * FROM food");
         
         await conection.end();
@@ -19,3 +12,25 @@ export async function GET(request) {
         console.log("erro conectar ao banco de dados", erro)
     }
 }
+
+// import mysql from "mysql2/promise";
+
+// export async function GET(request) {
+//     const dbConfig = {
+//         host: "localhost",
+//         user: "root",
+//         password: "1234",
+//         database: "foods"
+//     };
+
+//     try {
+
+//         const conection = await mysql.createConnection(dbConfig);
+//         const [rows] = await conection.execute("SELECT * FROM food");
+        
+//         await conection.end();
+//         return Response.json(rows);
+//     } catch (erro) {
+//         console.log("erro conectar ao banco de dados", erro)
+//     }
+// }
