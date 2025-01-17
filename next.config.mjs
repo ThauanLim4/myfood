fef7aaaad0a2b97b60a7f15fe65d66a7d6348b4f
ThauanLim4 <116463820+ThauanLim4@users.noreply.github.com>
2025-01-17 10:56:32 -0300

    teste43242

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
         {
             source: '/api/mysql/:path*',
             headers: [
+                { key: 'Access-Control-Allow-Credentials', value: 'true' },
                 { key: 'Access-Control-Allow-Origin', value: '*' },
                 { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE, OPTIONS' },
                 { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
