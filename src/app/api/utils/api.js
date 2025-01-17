fc59367f1ff826b37c4bcaf5b58822fb6d68ba80
ThauanLim4 <116463820+ThauanLim4@users.noreply.github.com>
2025-01-17 11:02:00 -0300

    ok

--- src/app/api/utils/api.js
+++ src/app/api/utils/api.js
@@ -1,9 +1,4 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:3000/api/mysql", headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization'
-    }
-});
+    baseURL: "http://localhost:3000/api/mysql",});
