a93f3ae0f27ea52171b471d85afb34ad07b95cd0
ThauanLim4 <116463820+ThauanLim4@users.noreply.github.com>
2025-03-05 08:25:58 -0300

    ok

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     headers: () => [
         {
-            source: '/api/mysql/:path*',
+            source: '/api/:path*',
             headers: [
                 { key: 'Access-Control-Allow-Credentials', value: 'true' },
                 { key: 'Access-Control-Allow-Origin', value: '*' },
