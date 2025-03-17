99d6be0b7ca8c3bff3924f4a5afb52c06128443b
ThauanLim4 <116463820+ThauanLim4@users.noreply.github.com>
2025-03-17 10:42:14 -0300

    FEAT: implementação do Swiper para carrosséis nas seções de categorias e lojas, melhorando a apresentação e navegação dos itens

--- src/app/components/home/Categories.jsx
+++ src/app/components/home/Categories.jsx
@@ -2,6 +2,8 @@
 import "@/app/globals.css";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 export const CategoriesComponent = () => {
     const itensCategoriesClass = "flex flex-col justify-center items-center text-center w-32 h-28";
     const [categoriesData, setCategoriesData] = useState([]);
@@ -17,19 +19,29 @@ export const CategoriesComponent = () => {
         <div>
             <div className="text-start">
                 <div className="flex items-center gap-3 w-full h-56 max-sm:overflow-x-scroll max-md:overflow-x-scroll max-lg:overflow-x-scroll">
-                    {categoriesData ?
-                        categoriesData.map((item, index) => {
-                            return (
-                                <div key={index} className="hover:shadow-sombrainterna">
-                                    <Link href={`categorias/${item.name.trim().toLocaleLowerCase()}`} className="grid grid-rows-0.7/0.3-rows items-center justify-center gap-3 bg-opacity-50 hover:bg-verdeclaro transition-all duration-300 rounded-lg">
-                                        <div className={`${itensCategoriesClass} rounded-b-full`}>
-                                            <img className="min-w-28 min-h-28" src={item.image} width={100} height={100} alt="" />
+                    <Swiper slidesPerView={2} spaceBetween={10} 
+                    breakpoints={
+                         {
+                            320: { slidesPerView: 3},
+
+                         }
+                    }>
+                        {categoriesData ?
+                            categoriesData.map((item, index) => {
+                                return (
+                                    <SwiperSlide key={index}>
+                                        <div className="hover:shadow-sombrainterna">
+                                            <Link href={`categorias/${item.name.trim().toLocaleLowerCase()}`} className="grid grid-rows-0.7/0.3-rows items-center justify-center gap-3 bg-opacity-50 hover:bg-verdeclaro transition-all duration-300 rounded-lg">
+                                                <div className={`${itensCategoriesClass} rounded-b-full`}>
+                                                    <img className="min-w-28 min-h-28 object-contain" src={item.image} width={100} height={100} alt="" />
+                                                </div>
+                                                <h3 className="font-light text-ellipsis hover:text-verdeescuro hover:cursor-pointer text-center">{item.name}</h3>
+                                            </Link>
                                         </div>
-                                        <h3 className="font-light text-ellipsis hover:text-verdeescuro hover:cursor-pointer text-center">{item.name}</h3>
-                                    </Link>
-                                </div>
-                            )
-                        }) : <></>}
+                                    </SwiperSlide>
+                                )
+                            }) : <></>}
+                    </Swiper>
                 </div>
             </div>
 
--- src/app/components/home/Stores.jsx
+++ src/app/components/home/Stores.jsx
@@ -2,6 +2,9 @@
 import "@/app/globals.css";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+
 export const StoresComponent = () => {
     const itensCategoriesClass = "flex flex-col justify-center items-center text-center w-32 h-28 mt-2";
     const [storesData, setStoresData] = useState([]);
@@ -17,19 +20,30 @@ export const StoresComponent = () => {
         <div>
             <div className="text-start">
                 <div className="flex items-center gap-3 w-full h-56 max-sm:overflow-x-scroll max-md:overflow-x-scroll max-lg:overflow-x-scroll">
-                    {storesData ?
-                        storesData.map((item, index) => {
-                            return (
-                                <div key={index} className="hover:shadow-sombrainterna">
-                                    <Link href={`pages/store/${item.name.trim().toLocaleLowerCase()}?storeid=${item.id}`} className="grid grid-rows-0.7/0.3-rows items-center justify-center gap-3 hover:bg-verdeclaro transition-all duration-300 rounded-lg">
-                                        <div className={`${itensCategoriesClass} rounded-b-full`}>
-                                            <img className="min-w-28 min-h-28 rounded-full" src={item.image} width={100} height={100} alt="" />
+                    <Swiper slidesPerView={2} spaceBetween={10} 
+                    breakpoints={
+                         {
+                            320: { slidesPerView: 3 },
+
+                         }
+                    }>
+
+                        {storesData ?
+                            storesData.map((item, index) => {
+                                return (
+                                    <SwiperSlide key={index}>
+                                        <div className="hover:shadow-sombrainterna mx-1">
+                                            <Link href={`pages/store/${item.name.trim().toLocaleLowerCase()}?storeid=${item.id}`} className="grid grid-rows-0.7/0.3-rows items-center justify-center gap-3 hover:bg-verdeclaro transition-all duration-300 rounded-lg">
+                                                <div className={`${itensCategoriesClass} rounded-b-full`}>
+                                                    <img className="min-w-28 min-h-28 rounded-full" src={item.image} width={100} height={100} alt="" />
+                                                </div>
+                                                <h3 className="font-light text-ellipsis hover:text-verdeescuro hover:cursor-pointer text-center">{item.name}</h3>
+                                            </Link>
                                         </div>
-                                        <h3 className="font-light text-ellipsis hover:text-verdeescuro hover:cursor-pointer text-center">{item.name}</h3>
-                                    </Link>
-                                </div>
-                            )
-                        }) : <></>}
+                                    </SwiperSlide>
+                                )
+                            }) : <></>}
+                    </Swiper>
                 </div>
             </div>
 
